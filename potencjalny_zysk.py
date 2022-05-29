1726a421df8212da919f4b16b37ea2e6b3ea78b4
LukiZiel <lzielinski@gmail.com>
2022-05-29 10:05:23 +0200

    Wersja 3.0 ostateczna- działająca

--- potencjalny_zysk.py
+++ potencjalny_zysk.py
@@ -2,13 +2,12 @@
 
 
 def potencjalny_zysk(wklad,procent,czas):
-    print("Witaj w programie obliczającym zysk z lokaty bankowej netto. \n")
+    # print("Witaj w programie obliczającym zysk z lokaty bankowej netto. \n")
     odsetki = (wklad * (procent/100) * czas)/360
     podatek = odsetki * 0.19
     zysk = round((odsetki - podatek), 2)
 
-    return zysk
-
+    return int(zysk)
 
 
 if __name__== "__main__":
