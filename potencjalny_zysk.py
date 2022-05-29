5ab4ad13cd1cbdd504ea9d4df08eb6affe64c3b8
LukiZiel <lzielinski@gmail.com>
2022-05-29 08:49:30 +0200

    Funkcja liczy oprocentowanie z lokat

--- potencjalny_zysk.py
+++ potencjalny_zysk.py
@@ -1,2 +1,17 @@
-def potencjalny_zysk(wklad, procent):
-    return wklad * procent
+#Program obliczający zyski z lokat bankowych
+
+
+def potencjalny_zysk():
+    print("Witaj w programie obliczającym zysk z lokaty bankowej netto.")
+    wklad = int(input("Podaj kwote jaką chesz zainwestować: "))
+    procent = int(input("Podaj oprocentowanie lokaty: "))/100
+    print(procent)
+    czas = int(input("Okres trwania lokaty(w dniach): "))
+    x = (wklad * procent * czas)/360
+    y = x * 0.19
+    z = x-y
+    print(y)
+
+    return z
+
+print(potencjalny_zysk())
