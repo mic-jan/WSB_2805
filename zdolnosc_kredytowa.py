0f68494c73cf1c1247e7282342cd70d1370b8ee1
mic-jan <michaljanski@gmail.com>
2022-05-29 08:45:38 +0200

    aktualizacja algorytmu w zdolnosc_kredytowa.py

--- zdolnosc_kredytowa.py
+++ zdolnosc_kredytowa.py
@@ -1,6 +1,6 @@
 def zdolnosc_kredytowa(wiek, plec, zarobki):
-    if (plec == "M")
+    if (plec == "M"):
         zdolnosc = ((65 - wiek) * 12 * zarobki) * 0,25
-    if (plec == "K")
+    if (plec == "K"):
         zdolnosc = ((60 - wiek) * 12 * zarobki) * 0,25
     return zdolnosc
