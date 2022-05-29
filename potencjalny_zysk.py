0e0de9629e73818b0aec3a899b8276f1d7642a1c
LukiZiel <lzielinski@gmail.com>
2022-05-29 09:54:45 +0200

    poprawiony blad z prcentami

--- potencjalny_zysk.py
+++ potencjalny_zysk.py
@@ -3,7 +3,7 @@
 
 def potencjalny_zysk(wklad,procent,czas):
     print("Witaj w programie obliczającym zysk z lokaty bankowej netto. \n")
-    odsetki = (wklad * procent * czas)/360
+    odsetki = (wklad * (procent/100) * czas)/360
     podatek = odsetki * 0.19
     zysk = round((odsetki - podatek), 2)
 
@@ -12,7 +12,7 @@ def potencjalny_zysk(wklad,procent,czas):
 
 
 if __name__== "__main__":
-    print(potencjalny_zysk(10,20,20))
+    print(potencjalny_zysk(100,5,180))
 
 
 
