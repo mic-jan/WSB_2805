bdd31f24dfc6742a8621d30a1ce239e534841b36
student <tester@tester-m>
2022-05-29 07:55:59 +0000

    sprawdzenie_zdrowia_v4

--- zdrowie.py
+++ zdrowie.py
@@ -1,17 +1,17 @@
 #Klient jest zdrowy kiedy:
 #puls jest od 60 do 100 uderzen na min
 #temperatura = 36,6
-#nalogi = 0
+#nalogi = TAK
 def sprawdzenie_zdrowia (puls, temperatura, nalogi):
-    if (puls <=100 and puls >=60 and temperatura == 36.6 and nalogi == 0):
+    if (puls <=100 and puls >=60 and temperatura == 36.6 and nalogi == "TAK"):
         print ("zdrowy")
     else: print ("chory")
 
-print ("wartosci: 80, 36.6,0")
-sprawdzenie_zdrowia(88,36.6,0)
+print ("wartosci: 80, 36.6,TAK")
+sprawdzenie_zdrowia(88,36.6,"TAK")
 
-print ("wartosci:50, 36.4,0")
-sprawdzenie_zdrowia(50,36.4,0)
+print ("wartosci:50, 36.4,NIE")
+sprawdzenie_zdrowia(50,36.4,"NIE")
 
-print ("wartosci:102, 36.8,1")
-sprawdzenie_zdrowia(102,36.8,1)
+print ("wartosci:102, 36.8,TAK")
+sprawdzenie_zdrowia(102,36.8,"TAK")
