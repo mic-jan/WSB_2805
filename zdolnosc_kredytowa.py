9cf9f7b4cbff55f84b77199636ae280eaccea04c
mic-jan <michaljanski@gmail.com>
2022-05-29 08:58:54 +0200

    Poprawka w zdolnosc_kredytowa

--- zdolnosc_kredytowa.py
+++ zdolnosc_kredytowa.py
@@ -1,6 +1,6 @@
 def zdolnosc_kredytowa(wiek, plec, zarobki):
-    if (plec == "M"):
-        zdolnosc = ((65 - wiek) * 12 * zarobki) * 0,25
-    if (plec == "K"):
-        zdolnosc = ((60 - wiek) * 12 * zarobki) * 0,25
-    return zdolnosc
+    if plec == "M":
+        zdolnosc = ((65 - wiek) * 12 * zarobki) * 0.25
+    elif plec == "K":
+        zdolnosc = ((60 - wiek) * 12 * zarobki) * 0.25
+    return int(zdolnosc)
