57c756d6baaf9fdc9d278410683c15e43813fb82
ingusek <ingusek@users.noreply.github.com>
2022-05-29 09:35:20 +0200

    euro_zloty zloty_euro

--- euro_zloty.py
+++ euro_zloty.py
@@ -1,15 +1,21 @@
 def przeliczWaluty():
     # Pobranie informacji co użytkownik chce przeliczyć i w jakiej ilości
     podajWalute = input("Podaj walutę do przeliczenia:\n")
-    ilosc = input("Podaj ilość PLN do przeliczenia na " + podajWalute + "\n")
+    ilosc = input("Podaj ilość do przeliczenia na " + podajWalute + "\n")
 
     # program przelicza PLN na euro
     if podajWalute == "PLN":
         wynik = int(ilosc) * 4.57
         print('%0.2f' % wynik)
-    else:
-        print("Error: Sproboj jeszcze raz.")
-        przeliczWaluty()
+        return wynik
 
 
-przeliczWaluty()
+    # program przelicza EUR na zloty
+    elif podajWalute == "EUR":
+        wynik2 = int(ilosc) / 4.57
+        print('%0.2f' % wynik2)
+        return wynik2
+
+if __name__ == "__main__":
+         przeliczWaluty()
+
